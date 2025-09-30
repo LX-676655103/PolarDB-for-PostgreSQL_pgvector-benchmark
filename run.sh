f4b7d1527bcc266bfb4c29c8c3cf24ce84d6cbe9
SuiHe <suihe.lx@alibaba-inc.com>
2025-09-30 22:30:49 +0800

    modify IVF index para

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@
 HNSW_M=16
 HNSW_EF_CONSTRUCTION=64
 # HNSW 查询参数
-ef_search_values=(200)
+ef_search_values=(10 20 30 50 80 100 120 140 160 180 200)
 
 # IVF 索引参数配置, 如果参数为空, 则不构建 IVF 索引
 # IVF_LISTS=2000
