ac5397e58e18ec8a87e858941166d3e6b8088535
SuiHe <suihe.lx@alibaba-inc.com>
2025-10-01 09:24:48 +0800

    modify IVF index para

--- run.sh
+++ run.sh
@@ -6,15 +6,15 @@
 # POLAR_BRANCH="POLARDB_15_STABLE"
 
 # HNSW 索引参数配置, 如果其中一个参数为空, 则不构建 HNSW 索引
-HNSW_M=16
-HNSW_EF_CONSTRUCTION=64
+# HNSW_M=16
+# HNSW_EF_CONSTRUCTION=64
 # HNSW 查询参数
 ef_search_values=(10 20 30 50 80 100 120 140 160 180 200)
 
 # IVF 索引参数配置, 如果参数为空, 则不构建 IVF 索引
 # IVF_LISTS=2000
-IVF_LISTS=
+IVF_LISTS=2000
 # IVF 查询参数
-# probes_values=(1 2 5 10 15 20 30 40 50 64 100)
+probes_values=(1 2 5 10 15 20 30 40 50 64 100)
 
 source /tcdata/run.sh
